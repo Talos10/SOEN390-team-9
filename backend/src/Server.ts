32bc590d19d4254ee82e6decbdc21f9981c3a3fe
Phong <phong1233@gmail.com>
2021-01-22 22:48:45 -0500

    implement docker and clean up code

--- backend/src/Server.ts
+++ backend/src/Server.ts
@@ -1,6 +1,5 @@
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
-import path from 'path';
 import helmet from 'helmet';
 
 import express, { NextFunction, Request, Response } from 'express';
@@ -34,7 +33,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // Add APIs
-app.use('/api', BaseController);
+app.use('/', BaseController);
 
 // Print API errors
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -43,20 +42,6 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     return res.status(BAD_REQUEST).json({
         error: err.message,
     });
-});
-
-
-
-/************************************************************************************
- *                              Serve front-end content
- ***********************************************************************************/
-
-const viewsDir = path.join(__dirname, 'views');
-app.set('views', viewsDir);
-const staticDir = path.join(__dirname, 'public');
-app.use(express.static(staticDir));
-app.get('*', (req: Request, res: Response) => {
-    res.sendFile('index.html', { root: viewsDir });
 });
 
 // Export express instance
