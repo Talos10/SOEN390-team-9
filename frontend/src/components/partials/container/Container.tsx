44a0c96f173a21dc01ff388e6c405965c7a21486
Tommy Josépovic <tommy.josepovic@gmail.com>
2021-01-28 09:24:43 -0500

    Add Appbar, Sidenav, and Material-UI

---
+++ frontend/src/components/partials/container/Container.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import Sidenav from '../sidenav/Sidenav';
+import Appbar from '../appbar/Appbar';
+
+import style from './Container.module.css';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function Container({ children }: Props) {
+  const [showSidenav, toggleSidenav] = useState<boolean>(false);
+
+  showSidenav
+    ? document.body.classList.add("sidenav-open")
+    : document.body.classList.remove("sidenav-open")
+
+  return (
+    <div className={style.Container}>
+      <Sidenav {...{ showSidenav, toggleSidenav }} />
+      <div>
+        <Appbar {...{ showSidenav, toggleSidenav }} />
+        <main className={style.main}>
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
