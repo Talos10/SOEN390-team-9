eb2317643bf91a355fec795ccbad4f95ae3c868f
Sebastien <sebastienbn8@gmail.com>
2021-02-01 15:16:55 -0500

    jwt key same as env

--- backend/config.ts
+++ backend/config.ts
@@ -8,7 +8,7 @@ const config = {
         name: process.env.MYSQL_DATABASE || 'soen_390_db',
         port: Number(process.env.MYSQL_PORT || 3306)
     },
-    jwt_public_key: process.env.JWT_PUBLIC_KEY || 'supersecretkey',
+    jwt_public_key: process.env.JWT_PUBLIC_KEY || 'NL5U0AbuR5RE7mDRWFWMUh5ArPa0WGww',
     logger: {
         level: process.env.LOG_LEVEL || 'debug',
     }
