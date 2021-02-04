e06882397aa1db1fa60fb1602964c53a33fd2643
unknown <sustaccosmin544@hotmail.ca>
2021-02-03 23:43:38 -0500

    Fixed merge in backend and frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import Main from './components/Main'
-
 import styles from './css/App.module.css'
-import Button from 'react-bootstrap/Button'
-import Container from './components/partials/container/Container';
 
 function App() {
   return (
