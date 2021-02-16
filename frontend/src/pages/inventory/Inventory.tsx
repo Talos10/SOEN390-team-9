ac22c649e8d4d7261ce844458223c61f8d398fb0
Tommy Josépovic <tommy.josepovic@gmail.com>
2021-02-16 16:55:55 -0500

    Finish Inventory Summary Front-End

--- frontend/src/pages/inventory/Inventory.tsx
+++ frontend/src/pages/inventory/Inventory.tsx
@@ -1,9 +1,16 @@
 import { Link } from 'react-router-dom';
 import { Button } from '@material-ui/core';
-import { Container } from '../../components';
+import { Container, Card } from '../../components';
 import './Inventory.scss';
 
 export default function Inventory() {
+
+  const items = [
+    { name: "Bike", quantity: 19, type: "Finished-Good", vendor: '' },
+    { name: "Steel", quantity: 10, type: "Raw Material", vendor: 'Mike\'s Steel Factory' },
+    { name: "Wheel", quantity: 28, type: "Semi-Finished Good", vendor: '' },
+  ]
+
   return (
     <Container>
       <div className="Inventory">
@@ -15,6 +22,28 @@ export default function Inventory() {
             </Button>
           </div>
         </div>
+        <Card className="summary">
+          <table>
+            <thead>
+              <tr>
+                <th>Item</th>
+                <th>Quantity</th>
+                <th>Type</th>
+                <th>Vendor</th>
+              </tr>
+            </thead>
+            <tbody>
+              {items.map(item =>
+                <tr key={item.name}>
+                  <td className="name">{item.name}</td>
+                  <td>{item.quantity}</td>
+                  <td>{item.type}</td>
+                  <td>{item.vendor}</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </Card>
       </div>
     </Container>
   );
