8fec60a90258c839df3bf4fcd86c2b023497282f
tommy-josepovic <tommy-josepovic@users.noreply.github.com>
2021-03-15 17:44:39 -0400

    Fixed home

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,11 +1,4 @@
-import { useSnackbar } from '../../contexts';
-
 export const Home = () => {
-  const snackbar = useSnackbar();
-  const testSnack = () => {
-    snackbar.push('What have you done!?', () => console.log('undid'));
-  };
-
   return <div>Welcome {localStorage.getItem('name')}.</div>;
 };
 
