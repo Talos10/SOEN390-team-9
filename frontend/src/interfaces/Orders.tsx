221e48ec03e04c0cb3de35bdb6b01829ab4a03f7
tommy-josepovic <tommy-josepovic@users.noreply.github.com>
2021-03-16 08:06:15 -0400

    Format

--- frontend/src/interfaces/Orders.tsx
+++ frontend/src/interfaces/Orders.tsx
@@ -1,18 +1,18 @@
 interface Orders {
-    orderId: number;
-    status: string;
-    cost: number;
-    creationDate: string;
-    startDate: string;
-    estimatedEndDate: string;
-    completionDate: string;
-    orderedGoods: OrderedGoods[];
+  orderId: number;
+  status: string;
+  cost: number;
+  creationDate: string;
+  startDate: string;
+  estimatedEndDate: string;
+  completionDate: string;
+  orderedGoods: OrderedGoods[];
 }
 
 interface OrderedGoods {
-    id: number;
-    cost: number;
-    quantity: number;
+  id: number;
+  cost: number;
+  quantity: number;
 }
 
 export type { Orders };
