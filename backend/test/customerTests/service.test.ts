3f0b93d48f95738bd93fca13598438b8da7afd7b
Sebastien <sebastienbn8@gmail.com>
2021-03-23 13:42:19 -0400

    FEAT-82: Fixed failing test

--- backend/test/customerTests/service.test.ts
+++ backend/test/customerTests/service.test.ts
@@ -12,7 +12,7 @@ sinonStubPromise(sinon);
 
 let sandbox: sinon.SinonSandbox;
 
-const mockCustomerModel = {
+const mockCustomerModel : CustomerModel= {
     customerId: 1,
     name: 'john',
     email: 'doe'
@@ -53,7 +53,7 @@ describe('Customer Service Test', () => {
         const customerService = new CustomerService();
         sandbox.stub(CustomerModel, 'addCustomer').resolves(1);
         const res = await customerService.createNewCustomer(mockCustomer.name, mockCustomer.email);
-        expect(res).to.equal(mockCustomerModel);
+        expect(res.customerId).to.equal(mockCustomerModel.customerId);
     });
 
     it('Test find customer by ID', async () => {
